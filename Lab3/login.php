0479c696c763eb2a3383d22b13a82ca9b19cc4e8
AhmedSakr54 <ahmedsakr3645@gmail.com>
2020-12-06 23:38:33 +0200

    finish

--- Lab3/login.php
+++ Lab3/login.php
@@ -8,7 +8,7 @@ include_once 'header.php';
         <input type="email" name="email" placeholder="Email" class="form-element" id="email"><br>
         <input type="password" name="password" placeholder="Password" class="form-element" id="password"><br>
         <button type="submit" name="submit" class="btn"><i class="fas fa-sign-in-alt"></i> Log In</button><br>
-        <p class="hide" id="err-paragraph"></p>
+        <p class="hide error" id="err-paragraph"></p>
     </form>
 </div>
 <script src="app.js"></script>
